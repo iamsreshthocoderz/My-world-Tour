0d101a285f3097c1724bcef6b6850a531698d82a
iamsreshthocoderz <sreshthot@gmail.com>
2023-12-14 03:16:54 +0600

    first commit

---
+++ src/components/Countries/Countries.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import country from "../Country/country";
+
+const Countries = () => {
+    const [countries,setCountries] = useState([]);
+    useEffect(()=>{
+        fetch("https://restcountries.com/v3.1/all")
+        .then(res => res.json())
+        .then(data => setCountries(data));
+    }, [])
+    return (
+        <div>
+            <h3>Countries: {countries.length}</h3>
+            {
+                countries.map(country =><country></country>)
+            }
+          
+        </div>
+    );
+};
+
+export default Countries;
---
+++ src/components/Country/Country.jsx
@@ -0,0 +1,10 @@
+const country = ({Country}) => {
+    console.log(Country);
+    return (
+        <div>
+            <h1>name</h1>
+        </div>
+    );
+};
+
+export default country;
